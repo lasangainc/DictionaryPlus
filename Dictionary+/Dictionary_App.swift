be5f50cf3eb167b4d2411d59655c366f4d207157
Benji <ben.jonasson@outlook.com>
2025-08-11 19:54:10 +0200

    Initial Commit

---
+++ Dictionary+/Dictionary_App.swift
@@ -0,0 +1,17 @@
+//
+//  Dictionary_App.swift
+//  Dictionary+
+//
+//  Created by Benji on 2025-08-11.
+//
+
+import SwiftUI
+
+@main
+struct Dictionary_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Dictionary+Tests/Dictionary_Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Dictionary_Tests.swift
+//  Dictionary+Tests
+//
+//  Created by Benji on 2025-08-11.
+//
+
+import Testing
+@testable import Dictionary_
+
+struct Dictionary_Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
